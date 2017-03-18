d2e6a04b767bc7fb2cc169396f9decad61cc9fde
Uziel Perez <uvperez@hotmail.com>
2017-03-17 22:59:15 -0300

    Initial commit

---
+++ src/main/java/com/daoautogen/Main.java
@@ -0,0 +1,10 @@
+package com.daoautogen;
+
+/**
+ * Created by uziel on 17/03/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
